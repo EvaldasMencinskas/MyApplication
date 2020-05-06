d52aaa3b4ca32b60dcd96d37749f65a913cc4bcb
EvaldasMencinskas <58887179+EvaldasMencinskas@users.noreply.github.com>
2020-05-06 18:13:19 +0300

    Initial commit

---
+++ app/src/main/java/com/example/myapplication/ui/main/SectionsPagerAdapter.java
@@ -0,0 +1,64 @@
+package com.example.myapplication.ui.main;
+
+import android.content.Context;
+
+import androidx.annotation.Nullable;
+import androidx.annotation.StringRes;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
+
+import com.example.myapplication.R;
+
+/**
+ * A [FragmentPagerAdapter] that returns a fragment corresponding to
+ * one of the sections/tabs/pages.
+ */
+public class SectionsPagerAdapter extends FragmentPagerAdapter {
+
+    @StringRes
+    private static final int[] TAB_TITLES = new int[]{R.string.tab_text_1, R.string.tab_text_2};
+    private final Context mContext;
+
+    public SectionsPagerAdapter(Context context, FragmentManager fm) {
+        super(fm);
+        mContext = context;
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        Fragment fragment=null;
+        switch (position) {
+            case 0:
+                fragment = new frag1();
+                break;
+            case 1:
+                fragment = new frag2();
+                break;
+            case 2:
+                fragment = new frag3();
+                break;
+        }
+        return fragment;
+    }
+
+    @Nullable
+    @Override
+    public CharSequence getPageTitle(int position) {
+        switch (position) {
+            case 0:
+                return "Contacts";
+            case 1:
+                return "Chat";
+            case 2:
+                return "Online";
+        }
+        return null;
+    }
+
+    @Override
+    public int getCount() {
+        // Show 2 total pages.
+        return 3;
+    }
+}
